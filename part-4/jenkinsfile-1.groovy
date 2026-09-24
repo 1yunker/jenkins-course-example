883b17872ff74b32efe52dacba624eee15298c32
Юрий Помазкин <ypomazkin@powertask.ru>
2026-09-24 16:59:39 +0500

    Lint and update

--- part-4/jenkinsfile-1.groovy
+++ part-4/jenkinsfile-1.groovy
@@ -1,7 +1,7 @@
 pipeline {
     agent any
     tools {
-        maven 'mvn_3_6_3'
+        maven 'M3'
     }
     triggers {
         cron('H/55 * * * 1-5')
@@ -9,7 +9,7 @@ pipeline {
     options {
         timestamps()
         buildDiscarder logRotator(daysToKeepStr: '7',
-                                   numToKeepStr: '14' )
+                                   numToKeepStr: '14')
         timeout(time: 10, unit: 'MINUTES')
     }
     stages {
